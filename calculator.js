052fa0b678504d60197ad55dda9b0b700a6cf154
Ron <ronmycode@gmail.com>
2020-10-29 21:38:06 -0500

    defining digits as vars on js

---
+++ calculator.js
@@ -0,0 +1,11 @@
+//defining digits
+var one = document.getElementById('N-one');
+var two = document.getElementById('N-two');
+var three = document.getElementById('N-three');
+var four = document.getElementById('N-four');
+var five = document.getElementById('N-five');
+var six = document.getElementById('N-six');
+var seven = document.getElementById('N-seven');
+var eight = document.getElementById('N-eight');
+var nine = document.getElementById('N-nine');
+var zero = document.getElementById('N-zero');
