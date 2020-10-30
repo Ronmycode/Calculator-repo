801cd322919068f5394470a116c549254d9943b0
Ron <ronmycode@gmail.com>
2020-10-29 21:54:43 -0500

    add event Listener test

--- calculator.js
+++ calculator.js
@@ -9,3 +9,11 @@ var seven = document.getElementById('N-seven');
 var eight = document.getElementById('N-eight');
 var nine = document.getElementById('N-nine');
 var zero = document.getElementById('N-zero');
+
+var digits = document.getElementById('digits');
+
+digits.addEventListener('click',works);
+
+function works(){
+	alert("works!");
+};
