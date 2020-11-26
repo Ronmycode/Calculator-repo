1dc3b6f9a1447b5eb865fd14babf94264e6f4e99
Ron <ronmycode@gmail.com>
2020-11-25 19:44:59 -0600

    dont work

--- lotto.js
+++ lotto.js
@@ -1,28 +1,28 @@
+dont work
+// var winner_numbers = 0;
+// var ticket = 0;
 
-var winner_numbers = [];
-var ticket = [];
+// function raffle(){
+//     while(winner_numbers.length <= 1){
+//     var num = Math.floor(Math.random() * 1); 
+//         winner_numbers.push(num);
+//         console.log(winner_numbers);
 
-function raffle(){
-    while(winner_numbers.length <= 1){
-    var num = Math.floor(Math.random() * 10); 
-        winner_numbers.push(num);
-        console.log(winner_numbers);
+//     }
+// }
 
-    }
-}
-
-function Gold_ticket(){
-        if (ticket !== winner_numbers){
-        while(ticket.length <= 1 ){
-        var x = Math.floor(Math.random() * 10); 
-            ticket.push(x);
-            console.log(ticket);
-        }
-    }
-}
+// function Gold_ticket(){
+//         while (ticket !== winner_numbers){
+//         while(ticket.length <= 1 ){
+//         var x = Math.floor(Math.random() * 1); 
+//             ticket.push(x);
+//             console.log(ticket);
+//         }
+//     }
+// }
 
 
 
-raffle();
-console.log("sorteo");
-Gold_ticket();
+// raffle();
+// console.log("sorteo");
+// Gold_ticket();
