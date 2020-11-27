647741c5ed69ce3039ab3b273acfc01ca54ec05a
Ron <ronmycode@gmail.com>
2020-11-27 16:01:58 -0600

    turning lotto into a slot machine

--- lotto.js
+++ lotto.js
@@ -1,42 +1,10 @@
-const winner = [0, 0]
-let ticket = [];
-
-// while(ticket !== winner){
-//     for (i=0; i<=1; i++){
-//         var num = Math.floor(Math.random() * 3);
-//         console.log(num);
-//         ticket.push(num);
-//         console.log(ticket);
-//     }
-// }
+//trying with a slot machine
 
 
+function pull (){
+    for(i=1; i<=3; i++){
+    console.log(Math.floor(Math.random() * 5));
+    }
+}
 
-// dont work
-// var winner_numbers = 0;
-// var ticket = 0;
-
-// function raffle(){
-//     while(winner_numbers.length <= 1){
-//     var num = Math.floor(Math.random() * 1); 
-//         winner_numbers.push(num);
-//         console.log(winner_numbers);
-
-//     }
-// }
-
-// function Gold_ticket(){
-//         while (ticket !== winner_numbers){
-//         while(ticket.length <= 1 ){
-//         var x = Math.floor(Math.random() * 1); 
-//             ticket.push(x);
-//             console.log(ticket);
-//         }
-//     }
-// }
-
-
-
-// raffle();
-// console.log("sorteo");
-// Gold_ticket();
+pull();
