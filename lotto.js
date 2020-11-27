da6f4036e1456d83344f4b5e22bf14276b87baf6
Ron <ronmycode@gmail.com>
2020-11-27 16:35:22 -0600

    create opbject for reels

--- lotto.js
+++ lotto.js
@@ -1,10 +1,22 @@
 //trying with a slot machine
 
 
+const reels = {
+    reel_one : 1,
+    reel_two : 2,
+    reel_three : 3
+};
+
+
+
+
 function pull (){
-    for(i=1; i<=3; i++){
-    console.log(Math.floor(Math.random() * 5));
+    for(i=0; i<=2; i++){
+    reels[i] = Math.floor(Math.random() * 5);
     }
 }
 
+
+
 pull();
+console.log(reels);
